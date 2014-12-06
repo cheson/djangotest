d05ef0f9437eba4f7290a3f571bf5ee4bfcffdef
Jason Chen <cheson@stanford.edu>
2014-12-06 00:29:14 -0800

    a white christmas, just another day

--- polls/models.py
+++ polls/models.py
@@ -6,18 +6,26 @@ from django.utils import timezone
 class Question(models.Model): #so theses fields are django classes that allow the database to be created. do we need to use fields?
     def __str__(self):
     	return self.question_text
+
     def was_published_recently(self):
     	return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-
 class Choice(models.Model):
+    def __str__(self):
+    	return self.choice_text
+
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    def __str__(self):
-    	return self.choice_text
 
 #Steps to making model changes:
 
